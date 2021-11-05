df2599e3494aef32386617d58ac0cd236a59dd71
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-11-05 20:54:18 +0600

    added function pors passRole

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -11,6 +11,18 @@ contract Token is ERC20 {
   constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
     minter = msg.sender; //only initially
   }
+  function passMinterRole(address mintRoleAddress) public returns (bool) {
+  	require(msg.sender==mintRoleAddress, 'Error, only owner can change pass minter role');
+  	minter = mintRoleAddress;
+
+    emit MinterChanged(msg.sender, mintRoleAddress);
+    return true;
+  }
+
+  function mint(address account, uint256 amount) public {
+		require(msg.sender==minter, 'Error, msg.sender does not have minter role'); //mintRoleAddress
+		_mint(account, amount);
+	}
 
   
 }
