2ee652a3c7b24481f10697cf069fff44d0633eb1
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-11-05 21:03:28 +0600

    function added for withdraw ,borrow following erc20 standard

--- src/contracts/SafeHand.sol
+++ src/contracts/SafeHand.sol
@@ -34,4 +34,59 @@ contract SafeHand {
     emit Deposit(msg.sender, msg.value, block.timestamp);
   }
 
+  function withdraw() public {
+    require(isDeposited[msg.sender]==true, 'Error, no previous deposit');
+    uint userBalance = etherBalanceOf[msg.sender]; //for event
+
+    //check user's hodl time
+    uint depositTime = block.timestamp - depositStart[msg.sender];
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interestPerSecond * depositTime;
+
+    //send funds to user
+    msg.sender.transfer(etherBalanceOf[msg.sender]); //eth back to user
+    token.mint(msg.sender, interest); //interest to user
+
+    //reset depositer data
+    depositStart[msg.sender] = 0;
+    etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
+
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
+  }
+
+  function borrow() payable public {
+    require(msg.value>=1e16, 'Error, collateral must be >= 0.01 ETH');
+    require(isBorrowed[msg.sender] == false, 'Error, loan already taken');
+
+    //this Ether will be locked till user payOff the loan
+    collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
+
+    //calc tokens amount to mint, 50% of msg.value
+    uint tokensToMint = collateralEther[msg.sender] / 2;
+
+    //mint&send tokens to user
+    token.mint(msg.sender, tokensToMint);
+
+    //activate borrower's loan status
+    isBorrowed[msg.sender] = true;
+
+    emit Borrow(msg.sender, collateralEther[msg.sender], tokensToMint);
+  }
+
+  function payOff() public {
+    require(isBorrowed[msg.sender] == true, 'Error, loan not active');
+    require(token.transferFrom(msg.sender, address(this), collateralEther[msg.sender]/2), "Error, can't receive tokens"); //must approve dBank 1st
+
+    uint fee = collateralEther[msg.sender]/10; //calc 10% fee
+
+    //send user's collateral minus fee
+    msg.sender.transfer(collateralEther[msg.sender]-fee);
+
+    //reset borrower's data
+    collateralEther[msg.sender] = 0;
+    isBorrowed[msg.sender] = false;
+
+    emit PayOff(msg.sender, fee);
+  }
 }
